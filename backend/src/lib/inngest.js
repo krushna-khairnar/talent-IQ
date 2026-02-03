5dd3718621735fff18e607e3c48f868be80a466b
Krushna Khairnar <krushnakhairnar536@gmail.com>
2026-02-03 18:21:01 +0530

    fix: remove stream import from inngest

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,7 +1,6 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
-import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({ id: "talent-iq" });
 
@@ -29,9 +28,7 @@ const deleteUserFromDB = inngest.createFunction(
   { event: "clerk/user.deleted" },
   async ({ event }) => {
     await connectDB();
-
-    const { id } = event.data;
-    await User.deleteOne({ clerkId: id });
+    await User.deleteOne({ clerkId: event.data.id });
   }
 );
 
