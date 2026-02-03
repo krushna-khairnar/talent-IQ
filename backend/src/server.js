c366167606aab89896fb94ea87104ebfb02c8038
Krushna Khairnar <krushnakhairnar536@gmail.com>
2026-02-03 18:09:18 +0530

    Clerk and Inngest syncing done

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,37 @@
 import express from "express";
+import path from "path";
+import cors from "cors";
+import { serve } from "inngest/express";
+import { clerkMiddleware } from "@clerk/express";
+
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
+import { inngest, functions } from "./lib/inngest.js";
 
 const app = express();
+const __dirname = path.resolve();
 
-app.get('/', (req, res) => {
-  res.json('Hello World!');
+// middleware
+app.use(express.json());
+// credentials:true meaning?? => server allows a browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
 });
 
-// Start the server and listen for incoming connections
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 const startServer = async () => {
   try {
     await connectDB();
