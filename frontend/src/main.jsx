1700f426671858fdac4a3f420d36b1fb37b41a5e
Krushna Khairnar <krushnakhairnar536@gmail.com>
2026-02-09 14:11:55 +0530

    Setting up frontend tools and libs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
 import { dark } from '@clerk/themes'
+import { BrowserRouter } from 'react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -12,13 +14,19 @@ if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
 }
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}
+    <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}
         appearance={{
         theme: dark,
-    }}>
-        <App />
-    </ClerkProvider>
+        }}>
+            <App />
+        </ClerkProvider>
+    </QueryClientProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
