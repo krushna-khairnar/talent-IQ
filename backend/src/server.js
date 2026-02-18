bef91d5b15d172122374f21e5b873015c3997019
Krushna Khairnar <krushnakhairnar536@gmail.com>
2026-02-18 18:06:03 +0530

    Prepare for Render deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -62,8 +62,7 @@ app.get("/health", (req, res) => {
 // make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("/{*any}", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
